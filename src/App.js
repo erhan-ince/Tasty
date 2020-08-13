998409a0091d835fce8e5e3bf0c41dbd29767c34
Erhan Ince <erhan.ince@live.de>
2020-08-13 13:18:35 +0200

    Tasty

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Home from './Components/Home';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(props) {
+     return (
+          <Router>
+               {props.children}
+               <div className='App'>
+                    <nav>
+                         <ul>
+                              <li>
+                                   <Link to='/'>All Beers</Link>
+                              </li>
+                              <li>
+                                   <Link to='/'>Random Beer</Link>
+                              </li>
+                         </ul>
+                    </nav>
+                    <Switch>
+                         <Route path='/' exact>
+                              <Home />
+                         </Route>
+                    </Switch>
+               </div>
+          </Router>
+     );
 }
-
 export default App;
---
+++ src/Components/Home.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+     state = {
+          data: [],
+     };
+     componentDidMount() {
+          fetch(
+               'https://api.spoonacular.com/recipes/complexSearch?type=main course&apiKey=adc6b8b2a9dd4856a79419fdcec21e21'
+          )
+               .then((response) => response.json())
+               .then((json) => {
+                    this.setState({ data: json });
+               });
+     }
+     render() {
+          return <div className='Article'></div>;
+     }
+}
+export default Home;
